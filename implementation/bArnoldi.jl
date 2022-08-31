d9b03a8769c23e0bb9362e285c93ba4b235a8e90
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2022-08-31 06:10:02 -0600

    Rename & comment

--- implementation/bArnoldi.jl
+++ implementation/bArnoldi.jl
@@ -8,23 +8,23 @@ iter(i, p) = (i-1)*p+1:i*p
 # Block Arnoldi
 function bArnoldi(A, X0, m, p)
     n = size(A, 1)
-    U = zeros(n, p*(m+1))
-    H = zeros(p*(m+1), p*m)
+    W = zeros(n, p*(m+1))   # W = [V1, ..., Vm+1]
+    H = zeros(p*(m+1), p*m) # H = [S1, ..., Sm]
     for i=1:p
-        U[:, i] = X0[:, i] / norm(X0[:, i])
+        W[:, i] = X0[:, i] / norm(X0[:, i])
     end
     for j in 1:m
         iterJ = iter(j, p)
-        W = A * U[:, iterJ]
+        zz = A * W[:, iterJ]
         for i in 1:j
             iterI = iter(i, p)
-            H[iterI, iterJ] = U[:, iterI]' * W
-            W -= U[:, iterI] * H[iterI, iterJ]
+            H[iterI, iterJ] = W[:, iterI]' * zz
+            zz -= W[:, iterI] * H[iterI, iterJ]
         end
         iterJp1 = iter(j+1, p)
-        Q, R = qr(W)
-        U[:, iterJp1] = Q[:, 1:p]
-        H[iterJp1, iterJ] = R
+        V, S = qr(zz)
+        W[:, iterJp1] = V[:, 1:p]
+        H[iterJp1, iterJ] = S
     end
-    U, H
+    W, H
 end
