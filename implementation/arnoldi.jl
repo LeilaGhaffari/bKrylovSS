2e025f4a2d89628eb4eca3a2d53986135eee4f1a
Jed Brown <jed@jedbrown.org>
2022-09-06 15:25:21 -0600

    arnoldi: Handle early break-down/running to completion

--- implementation/arnoldi.jl
+++ implementation/arnoldi.jl
@@ -1,14 +1,17 @@
 # Arnoldi process
-function arnoldi(A, u, m)
+function arnoldi(A, u, m; tol=1e-12)
     n = size(A, 1)
-    Q = zeros(n, m+1)
-    H = zeros(m+1, m)
+    Q = zeros(n, min(n, m + 1))
+    H = zeros(min(n, m + 1), m)
     Q[:, 1] = u / norm(u)
     for j in 1:m
-        v = A*Q[:, j]
+        v = A * Q[:, j]
         for i in 1:j
             H[i, j] = Q[:, i]' * v
             v -= H[i, j] * Q[:, i]
+        end
+        if norm(v) < tol
+            break
         end
         H[j+1, j] = norm(v)
         Q[:, j+1] = v / H[j+1, j]
